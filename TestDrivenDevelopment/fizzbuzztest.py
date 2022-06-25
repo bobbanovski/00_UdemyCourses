7959b46cd2ce5abbd117b416a33a046cef93d3be
rc-paradyn <robert.coleman@paradyn.com.au>
2022-06-25 19:16:32 +1000

    Red Phase - failing unit test for returns2With2PassedIn

--- TestDrivenDevelopment/fizzbuzztest.py
+++ TestDrivenDevelopment/fizzbuzztest.py
@@ -1,8 +1,12 @@
 import pytest
 
 def fizzBuzz(value):
-    return("1")
+    return "1"
 
 def test_returns1With1PassedIn():
     retVal = fizzBuzz(1)
     assert retVal == "1"
+
+def test_returns2With2PassedIn():
+    retVal = fizzBuzz(2)
+    assert retVal == "2"
