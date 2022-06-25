5723e3d3fd04b64d4238fdd704ec2d1c3caccc27
rc-paradyn <robert.coleman@paradyn.com.au>
2022-06-25 22:32:31 +1000

    REFACTOR phase - use common function for testing

--- TestDrivenDevelopment/fizzbuzztest.py
+++ TestDrivenDevelopment/fizzbuzztest.py
@@ -3,10 +3,12 @@ import pytest
 def fizzBuzz(value):
     return str(value)
 
+def checkFizzBuzz(value, expectedRetVal):
+    retVal = fizzBuzz(value)
+    assert retVal == expectedRetVal
+
 def test_returns1With1PassedIn():
-    retVal = fizzBuzz(1)
-    assert retVal == "1"
+    checkFizzBuzz(1, "1")
 
 def test_returns2With2PassedIn():
-    retVal = fizzBuzz(2)
-    assert retVal == "2"
+    checkFizzBuzz(2, "2")
