6332dbb4f81ea8043266d7d521e751341cb821c5
rc-paradyn <robert.coleman@paradyn.com.au>
2022-06-25 15:53:20 +1000

    Update fizzbuzztest.py
    Green to Refactor Phase

--- TestDrivenDevelopment/fizzbuzztest.py
+++ TestDrivenDevelopment/fizzbuzztest.py
@@ -3,12 +3,6 @@ import pytest
 def fizzBuzz(value):
     return("1")
 
-def test_canAssertTrue():
-    assert True
-
-def test_canCallFizzBuzz():
-    fizzBuzz(1)
-
 def test_returns1With1PassedIn():
     retVal = fizzBuzz(1)
     assert retVal == "1"
