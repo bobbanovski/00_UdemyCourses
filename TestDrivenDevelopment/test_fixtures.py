564715098739bf4260d61b7b86be08819e9a4abf
rc-paradyn <robert.coleman@paradyn.com.au>
2022-06-27 10:36:28 +1000

    Fixture - yield keyword

--- TestDrivenDevelopment/test_fixtures.py
+++ TestDrivenDevelopment/test_fixtures.py
@@ -3,6 +3,10 @@ import pytest
 @pytest.fixture(autouse=True)
 def setup():
     print("\nSetup")
+    # Yield keyword - used when Fixture goes out of scope (teardown)
+    # in this case used once per test
+    yield
+    print("\nTeardown")
 
 # def test1(setup):
 def test1():
