4efb638aff4cb1974f010f67042602cbb331caaa
rc-paradyn <robert.coleman@paradyn.com.au>
2022-06-27 10:49:16 +1000

    Fixtures - request context with finalisers
    Allows the specification of teardown functions to each test, with multiple functions within a teardown call

--- TestDrivenDevelopment/test_fixtures.py
+++ TestDrivenDevelopment/test_fixtures.py
@@ -1,19 +1,34 @@
 import pytest
 
-@pytest.fixture(autouse=True)
-def setup():
-    print("\nSetup")
+# @pytest.fixture(autouse=True)
+@pytest.fixture()
+def setup1():
+    print("\nSetup 1")
     # Yield keyword - used when Fixture goes out of scope (teardown)
     # in this case used once per test
     yield
-    print("\nTeardown")
+    print("\nTeardown1")
+
+@pytest.fixture()
+def setup2(request):
+    print("\nSetup 2")
+
+    def teardown_a():
+        print("\nTeardown A")
+
+    def teardown_b():
+        print("\nTeardown B")
+
+    request.addfinalizer(teardown_a)
+    request.addfinalizer(teardown_b)
 
 # def test1(setup):
-def test1():
+def test1(setup1):
     print("\nExecuting test1")
     assert True
 
 # @pytest.mark.usefixtures("setup")
-def test2():
+
+def test2(setup2):
     print("\nExecuting test2")
     assert True
