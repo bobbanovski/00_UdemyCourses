c749bbf4d904ebc3ac8e2be4bd4fa3bde552f37b
rc-paradyn <robert.coleman@paradyn.com.au>
2022-06-27 10:19:03 +1000

    Fixtures - autouse applied to all functions in scope

--- TestDrivenDevelopment/test_fixtures.py
+++ TestDrivenDevelopment/test_fixtures.py
@@ -1,14 +1,15 @@
 import pytest
 
-@pytest.fixture()
+@pytest.fixture(autouse=True)
 def setup():
     print("\nSetup")
 
-def test1(setup):
+# def test1(setup):
+def test1():
     print("\nExecuting test1")
     assert True
 
-@pytest.mark.usefixtures("setup")
+# @pytest.mark.usefixtures("setup")
 def test2():
     print("\nExecuting test2")
     assert True
