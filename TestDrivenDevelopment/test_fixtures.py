5d15ff59cb496c40375f066434a834f53cb16f9e
rc-paradyn <robert.coleman@paradyn.com.au>
2022-06-27 10:10:18 +1000

    Fixtures - tests not linked to setup fixture

---
+++ TestDrivenDevelopment/test_fixtures.py
@@ -0,0 +1,13 @@
+import pytest
+
+@pytest.fixture()
+def setup():
+    print("\nSetup")
+
+def test1():
+    print("\nExecuting test1")
+    assert True
+
+def test2():
+    print("\nExecuting test2")
+    assert True
