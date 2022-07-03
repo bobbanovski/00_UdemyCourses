490566b3f2d6c892deef06ee3152321259891959
bobbanovski@gmail.com <bobbanovski@gmail.com>
2022-07-03 22:29:16 +1000

    Refactor - removed redundant tests

--- TestDrivenDevelopment/SupermarketKata/test_checkout.py
+++ TestDrivenDevelopment/SupermarketKata/test_checkout.py
@@ -1,12 +1,6 @@
 import pytest
 from Checkout import Checkout
 
-def test_AssertTrue():
-    assert True
-
-def test_CanInstantiateCheckout():
-    co = Checkout()
-
 def test_CanAddItemPrice():
     co = Checkout()
     co.addItemPrice("a", 1)
